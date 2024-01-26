f45fa780f10939ee68a128f99ce5edda98ec022a
alphacpc <alphacpc@gmail.com>
2024-01-26 10:31:10 +0000

    change config vagrantfile

--- vagrant/lab1-bootcamp/jsonplaceholder-app/main.py
+++ vagrant/lab1-bootcamp/jsonplaceholder-app/main.py
@@ -40,4 +40,4 @@ if __name__=='__main__':
     with app.app_context():
         db.create_all()
         # pass
-    app.run(host=os.getenv('IP'),debug=True,port=5000)
+    app.run(host=os.getenv("IP"),debug=True,port=os.getenv("PORT"))

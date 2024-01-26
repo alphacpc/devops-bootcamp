547bc164f606befebd3581486bcc868c9598dfea
alphacpc <alphacpc@gmail.com>
2024-01-26 10:01:15 +0000

    debug port listenning

--- vagrant/lab1-bootcamp/jsonplaceholder-app/main.py
+++ vagrant/lab1-bootcamp/jsonplaceholder-app/main.py
@@ -37,8 +37,7 @@ def not_found(error):
 
 if __name__=='__main__':
     # create_all_tables()
-    
     with app.app_context():
         db.create_all()
         # pass
-    app.run(debug=True,port=5000)
+    app.run(host=os.getenv('IP'),debug=True,port=5000)

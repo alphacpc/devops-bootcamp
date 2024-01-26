dfaf83885669eea72358d96a85f827e3d72e3c63
alphacpc <alphacpc@gmail.com>
2024-01-26 11:19:48 +0000

    start crontab database

---
+++ vagrant/lab1-bootcamp/backup/backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Configuration des variables
+DB_USER="alphacpc"
+DB_NAME="app"
+BACKUP_DIR="/home/alphacpc/Projects/backup"
+DATE=$(date +%Y%m%d%H%M%S)
+BACKUP_FILE="$BACKUP_DIR/$DB_NAME-$DATE.sql"
+
+# Définir le mot de passe PostgreSQL dans une variable d'environnement
+export PGPASSWORD="alphacpc"
+
+# Commande de sauvegarde
+pg_dump -U $DB_USER $DB_NAME > $BACKUP_FILE
+
+# Vérifier si la sauvegarde a réussi
+if [ $? -eq 0 ]; then
+    echo "Sauvegarde de la base de données réussie : $BACKUP_FILE"
+else
+    echo "Erreur lors de la sauvegarde de la base de données"
+fi
+
+# Effacer le mot de passe de l'environnement après la sauvegarde
+unset PGPASSWORD
